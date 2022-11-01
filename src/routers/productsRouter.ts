2e2c34357a315433afcadeb90f7454649ed6f341
Rafael Longo <rafaelongo45@gmail.com>
2022-11-01 14:11:09 -0300

    Feat: Creates schema for product update

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
+import productSchema from "../schemas/productSchema.js";
+import { validateSchema } from "../middlewares/schemaValidator.js";
 import productsController from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/products", productsController.getAllProducts);
 productsRouter.get("/products/:code", productsController.getProductByCode);
-productsRouter.put("/products/:code", productsController.updateProduct);
+productsRouter.put("/products/:code", validateSchema(productSchema), productsController.updateProduct);
 productsRouter.delete("/products/:code", productsController.deleteProduct);
 
 export default productsRouter;
